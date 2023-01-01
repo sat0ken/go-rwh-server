6d68d16ef76a079c6bd6aae89f90c757313f8edc
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-01-01 21:01:41 +0900

    update

--- client/client.go
+++ client/client.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"bytes"
 	"flag"
+	"fmt"
 	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"net/http/cookiejar"
+	"net/http/httputil"
+	"net/textproto"
 	"net/url"
 	"os"
 	"strings"
 )
 
-var server = "http://localhost:18888"
+var server = "http://127.0.0.1:18888"
+var githuburl = "http://github.com"
+var multipartdata stringFlags
+
+type stringFlags []string
+
+func (v *stringFlags) String() string {
+	return fmt.Sprintf("%v", multipartdata)
+}
+
+func (s *stringFlags) Set(v string) error {
+	*s = append(*s, v)
+	return nil
+}
 
 func parsedataUrlEncode(urlstr string) url.Values {
 	tmp := strings.Split(urlstr, "=")
@@ -76,15 +95,124 @@ func contentpost(file string) {
 	log.Println("Status :", resp.Status)
 }
 
+// 例3-10 Go言語でマルチパートフォームをPOST送信
+func postMultipart(data []string) {
+	var buffer bytes.Buffer
+	writer := multipart.NewWriter(&buffer)
+	for _, v := range data {
+		tmp := strings.Split(v, "=")
+		if tmp[0] == "name" {
+			writer.WriteField(tmp[0], tmp[1])
+		} else {
+			fileWriter, err := writer.CreateFormFile(tmp[0], tmp[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			readFile, err := os.Open(tmp[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer readFile.Close()
+			io.Copy(fileWriter, readFile)
+			writer.Close()
+		}
+	}
+	resp, err := http.Post(server, writer.FormDataContentType(), &buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Status:", resp.Status)
+}
+
+// 例3-11　送信するファイルに任意のMIMEタイプを設定
+func postMIME(data []string) {
+	var buffer bytes.Buffer
+	writer := multipart.NewWriter(&buffer)
+
+	for _, v := range data {
+		tmp := strings.Split(v, "=")
+		if tmp[0] == "thumbnaill" {
+			part := make(textproto.MIMEHeader)
+			part.Set("Content-Type", "image/jpeg")
+			part.Set("Content-Disposition", `form-data; name="thumbnaill"; filename="photo.jpg"`)
+			fileWriter, err := writer.CreatePart(part)
+			if err != nil {
+				log.Fatal(err)
+			}
+			readFile, err := os.Open("photo.jpg")
+			if err != nil {
+				log.Fatal(err)
+			}
+			io.Copy(fileWriter, readFile)
+			writer.Close()
+		}
+	}
+
+	resp, err := http.Post(server, writer.FormDataContentType(), &buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Status:", resp.Status)
+}
+
+// 例3-12 http.Client構造体を使用して、クッキーの送受信を行う
+func sendCookie() {
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := http.Client{
+		Jar: jar,
+	}
+	for i := 0; i < 2; i++ {
+		resp, err := client.Get(server + "/cookie")
+		if err != nil {
+			log.Fatal(resp)
+		}
+		dump, err := httputil.DumpResponse(resp, true)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(string(dump))
+	}
+}
+
+func proxy(proxyto string) {
+	proxyUrl, err := url.Parse(proxyto)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyUrl),
+		},
+	}
+	resp, err := client.Get(githuburl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(dump))
+}
+
 func main() {
 	var ishead bool
 	var dataUrlEncode string
 	var postValue string
 	var postContent string
+	var cookie string
+	var proxyurl string
+
 	flag.BoolVar(&ishead, "head", false, "use HTTP HEAD")
 	flag.StringVar(&dataUrlEncode, "data-urlencode", "", "data for URL Encode")
 	flag.StringVar(&postValue, "d", "", "data for POST")
 	flag.StringVar(&postContent, "T", "", "file data for POST")
+	flag.Var(&multipartdata, "F", "send multipart form data")
+	flag.StringVar(&cookie, "b", "", "cookie value")
+	flag.StringVar(&proxyurl, "x", "", "set proxy URL")
 	flag.Parse()
 
 	if ishead {
@@ -98,6 +226,13 @@ func main() {
 			post(values)
 		} else if len(postContent) != 0 {
 			contentpost(postContent)
+		} else if len(multipartdata) != 0 {
+			postMultipart(multipartdata)
+			postMIME(multipartdata)
+		} else if len(cookie) != 0 {
+			sendCookie()
+		} else if len(proxyurl) != 0 {
+			proxy(proxyurl)
 		}
 	}
 
--- http-1.0.go
+++ http-1.0.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
 	"syscall"
 	"time"
 )
@@ -13,6 +16,68 @@ const (
 )
 
 var contentType = []byte(`Content-Type: text/html; charset=utf-8`)
+
+type httpHeader struct {
+	method         string
+	path           string
+	version        string
+	host           string
+	authorization  map[string]string
+	userAgent      string
+	accept         string
+	acceptEncoding string
+	contentLength  int
+	contentType    string
+	content        string
+}
+
+func splitHeaderStr(headerstr string) string {
+	tmp := strings.Split(headerstr, ": ")
+	return tmp[1]
+}
+
+func authBasicHeader(basicstr string) {
+	tmp := strings.Split(basicstr, " ")
+	decode, _ := base64.StdEncoding.DecodeString(tmp[1])
+	tmp = strings.Split(string(decode), ":")
+	fmt.Printf("encoding is user is %s, pass is %s\n", tmp[0], tmp[1])
+}
+
+func parseBuf(recv string) httpHeader {
+	var header httpHeader
+	http := strings.Split(recv, "\r\n")
+	// fmt.Printf("recv http is %+v\n", http)
+
+	for _, v := range http {
+		if strings.Contains(v, "HTTP/") {
+			tmp := strings.Split(v, " ")
+			header.method = tmp[0]
+			header.path = tmp[1]
+			header.version = tmp[2]
+		} else if strings.Contains(v, "Host") {
+			header.host = splitHeaderStr(v)
+		} else if strings.Contains(v, "Authorization") {
+			authBasicHeader(splitHeaderStr(v))
+		} else if strings.Contains(v, "User-Agent") {
+			header.userAgent = splitHeaderStr(v)
+		} else if strings.Contains(v, "Accept:") {
+			header.accept = splitHeaderStr(v)
+		} else if strings.Contains(v, "Accept-Encoding:") {
+			header.acceptEncoding = splitHeaderStr(v)
+		} else if strings.Contains(v, "Content-Length") {
+			length := splitHeaderStr(v)
+			header.contentLength, _ = strconv.Atoi(length)
+		} else if strings.Contains(v, "Content-Type") {
+			header.contentType = splitHeaderStr(v)
+		} else {
+			if header.contentLength == len(v) {
+				header.content = v
+			}
+		}
+	}
+	fmt.Printf("header is %+v\n", header)
+	return header
+}
 
 func http1_0() {
 
@@ -29,10 +94,14 @@ func http1_0() {
 		}
 		recvbuf := make([]byte, 1500)
 		n, clientsa, err := syscall.Recvfrom(nfd, recvbuf, 0)
-		fmt.Printf("%s\n", string(recvbuf[:n]))
+
+		httpcontent := parseBuf(string(recvbuf[:n]))
 
 		// HTTPレスポンスの作成
 		b.Write([]byte(fmt.Sprintf("HTTP/1.0 %d OK\r\n", OK)))
+		if httpcontent.path == "/cookie" {
+			b.Write([]byte(fmt.Sprintf("Set-Cookie: LAST_ACCESS_TIME=%s\r\n", t.Format("03:04:05"))))
+		}
 		b.Write([]byte(fmt.Sprintf("Date: %s\r\n", t.Format(time.RFC1123))))
 		b.Write([]byte(fmt.Sprintf("Content-Length: %d\r\n", len(hello))))
 		b.Write(contentType)
